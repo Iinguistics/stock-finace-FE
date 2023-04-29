de7a2e0100d2bed8ff60ebaeae94f295124f7610
James <Thylinguistics@gmail.com>
2023-04-29 15:16:44 -0700

    adds ticker from params

--- src/app/details/detailspage/detailspage.component.ts
+++ src/app/details/detailspage/detailspage.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailspage',
   templateUrl: './detailspage.component.html',
-  styleUrls: ['./detailspage.component.css']
+  styleUrls: ['./detailspage.component.css'],
 })
 export class DetailspageComponent {
+  constructor(private route: ActivatedRoute) {}
 
+  private getTicker(): string {
+    const ticker = this.route.snapshot.paramMap.get('ticker');
+
+    if (!ticker) {
+      throw new Error('no ticker found.');
+    }
+
+    return ticker;
+  }
 }
